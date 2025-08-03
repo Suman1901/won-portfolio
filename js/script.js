b762848d5a67be7b903d1bffcd5b30cedcbcbf9d
Suman1901 <sumansahoo41243@gmail.com>
2025-08-03 21:55:01 +0530

    some animation add

---
+++ js/script.js
@@ -0,0 +1,33 @@
+function loader(){
+    let tl = gsap.timeline();
+
+    tl.from("#loader h3",{
+        x: 20,
+        opacity: 0,
+        duration: 0.8,
+        stagger: 0.2 
+    })
+
+    tl.to("#loader h3",{
+        opacity: 0,
+        x: -20,
+        duration: 0.8,
+        stagger: 0.2
+    })
+    tl.to("#loader", {
+        opacity: 0,
+    })
+    tl.from(".name span",{
+        y: -100,
+        opacity:0,
+        stagger: 0.1,
+        duration: 0.5,
+        delay: -0.5,
+    })
+    tl.to("#loader", {
+        display: "none"
+    })
+}
+
+loader()
+
